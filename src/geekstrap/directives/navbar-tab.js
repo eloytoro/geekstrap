627432b0acbeae099f5b352b5529d2ef78d7255b
Eloy <Eloy@Eloy.local>
2014-09-18 22:21:03 -0430

    caret

--- src/geekstrap/directives/navbar-tab.js
+++ src/geekstrap/directives/navbar-tab.js
@@ -1,22 +1,21 @@
 angular.module('geekstrap')
 
 .directive('navbarTab', function() {
-    return {
-      restrict:'E',
-      require: '^navbar',
-      replace: true,
-      transclude: true,
-      templateUrl: 'src/geekstrap/templates/navbar-tab.html',
-      scope: {
-        link: '@'
-      },
-      link: function (scope, element, attrs, controller) {
-        scope.width = $(element).width();
-        scope.id = controller.addTab(scope);
-        console.log(scope.offset);
-        element.on('click', function() {
-          controller.setActive(scope.id);
-        });
-      }
+  return {
+    restrict:'E',
+    require: '^navbar',
+    replace: true,
+    transclude: true,
+    templateUrl: 'src/geekstrap/templates/navbar-tab.html',
+    scope: {
+      link: '@'
+    },
+    link: function (scope, element, attrs, controller) {
+      scope.width = $(element).width();
+      scope.id = controller.addTab(scope);
+      element.on('click', function() {
+        controller.setActive(scope.id);
+      });
+    }
   };
 });
